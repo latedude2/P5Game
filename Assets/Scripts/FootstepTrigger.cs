c60e60fc59a62188f73632f54a63980df1511e28
Jokubas <jokubas@shape.dk>
2020-10-25 14:05:39 +0100

    Implemented FootstepTrigger and modified FirstPersonController for changing the footstep and jump/land sounds when hitting some triggers on the map (for entering grass/stone areas)

---
+++ Assets/Scripts/FootstepTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FootstepTrigger : MonoBehaviour
+{
+    [SerializeField] private FirstPersonController playerController;
+
+    [SerializeField] private AudioClip[] footstepSounds;
+    [SerializeField] private AudioClip jumpSound; // the sound played when character leaves the ground.
+    [SerializeField] private AudioClip landSound; // the sound played when character touches back on ground.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (footstepSounds != null && footstepSounds.Length > 0) playerController.SetFootstepSounds(footstepSounds);
+        if(jumpSound != null) playerController.SetJumpSound(jumpSound);
+        if(landSound != null) playerController.SetLandSound(landSound);
+
+    }
+}
