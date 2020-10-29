cc1ef096819e2e96e058ea0866ede00c8aaed72d
latedude2 <simasc10@gmail.com>
2020-10-29 10:29:51 +0100

    Create end test script

---
+++ Assets/Scripts/EndTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTest : MonoBehaviour
+{
+    [System.NonSerialized] public bool endTaskCompleted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
