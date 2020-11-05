90369ff500daf719fd15a93f5d464a9d517845d9
Jokubas <jokubas@shape.dk>
2020-11-05 11:41:46 +0100

    initial script

---
+++ Assets/Scripts/IntroSceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IntroSceneChange : MonoBehaviour
+{
+
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
