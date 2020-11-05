edb516c1190e31a99260c0f90bec29e49dfa613d
Jokubas <jokubas@shape.dk>
2020-11-05 14:12:22 +0100

    added VideoPlayerController script and added it to the IntroScene camera

---
+++ Assets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour
+{
+
+    [SerializeField] private string masterSceneName;
+
+    private VideoPlayer videoPlayer;
+    private long playerFrameCount;
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        playerFrameCount = Convert.ToInt64(videoPlayer.frameCount);
+        videoPlayer.Play();
+
+        //invoke method every .1f second from 0f time
+        InvokeRepeating("CheckOver", 0f, .1f);
+    }
+
+    private void CheckOver()
+    {
+        long playerCurrentFrame = videoPlayer.frame;
+
+        if (playerCurrentFrame >= playerFrameCount)
+        {
+            videoPlayer.Stop();
+            SceneManager.LoadScene(masterSceneName);
+            //Cancel Invoke since video is no longer playing
+            CancelInvoke("CheckOver");
+        }
+    }
+}
