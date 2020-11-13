9c6b281b97bc1bfcab8557f2443c4598546cb706
Jokubas <jokubas@shape.dk>
2020-11-13 15:27:27 +0100

    Camera shake to HurtEffect added

---
+++ Assets/Scripts/HurtEffect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEffect : MonoBehaviour
+{
+    private readonly float SHAKE_DEFORM_COEFFICIENT = .2f;
+
+    [SerializeField] private bool shake; //only for testing purposes
+
+    [SerializeField] private bool isShaking;
+    [SerializeField] private float shakeIntensity;
+    [SerializeField] private float shakeDecay;
+    private float currentShakeIntensity;
+    private Vector3 originalPos;
+    private Quaternion originalRot;
+
+    void Update()
+    {
+        TestHit();
+
+        if (currentShakeIntensity > 0)
+        {
+            transform.position = originalPos + Random.insideUnitSphere * shakeIntensity;
+            transform.rotation = new Quaternion(
+               GetDeformedRotation(originalRot.x),
+                GetDeformedRotation(originalRot.y),
+                GetDeformedRotation(originalRot.z),
+                GetDeformedRotation(originalRot.w)
+            );
+
+            currentShakeIntensity -= shakeDecay;
+        }
+        else if (isShaking)
+        {
+            isShaking = false;
+        }
+    }
+
+    private float GetDeformedRotation(float axisValue)
+    {
+        return axisValue + Random.Range(-shakeIntensity, shakeIntensity) * SHAKE_DEFORM_COEFFICIENT;
+    }
+
+    private void TestHit()
+    {
+        if (shake)
+        {
+            Hit();
+            shake = false;
+        }
+    }
+
+    // call this function from the follower's (bee swarm) script, when the distance is close enough
+    public void Hit()
+    {
+        originalPos = transform.position;
+        originalRot = transform.rotation;
+
+        currentShakeIntensity = shakeIntensity;
+        isShaking = true;
+    }
+}
