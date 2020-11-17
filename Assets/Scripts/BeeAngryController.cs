f8bd0468b329e4ef21c751adeadaf6fef43220f5
Mikkel Munk <43847789+TheZode@users.noreply.github.com>
2020-11-17 12:21:05 +0100

    Angry bee behaviour

---
+++ Assets/Scripts/BeeAngryController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class BeeAngryController : MonoBehaviour
+{
+    public Transform player;
+
+    private NavMeshAgent agent;
+    private int refreshTargetTimer = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.stoppingDistance = 2;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+        refreshTargetTimer -= 1;
+
+        //Calculation for bee relative speed. We can discuss what this should be.
+        agent.speed = 25 * (dist * 0.01f);
+        if (agent.speed < 2)
+            agent.speed = 2;
+
+        //we set destination for target to run less than every frame, cause it's heavy over longer distances
+        if (refreshTargetTimer <= 0)
+        {
+            agent.destination = player.position;
+            refreshTargetTimer = 50;
+        }
+
+        //add timer
+        if (dist <= 2)
+        {
+            BeeAttack();
+        }
+    }
+
+    void BeeAttack()
+    {
+        //Set bee run attack animation here
+
+        //player.GetComponent<HurtEffect>().Hit();
+        Debug.Log("DEAD!");
+    }
+}
--- Assets/Scripts/EndTest.cs
+++ Assets/Scripts/EndTest.cs
@@ -8,8 +8,9 @@ public class EndTest : MonoBehaviour
     [System.NonSerialized] public bool endTaskCompleted = false;
     [System.NonSerialized] public string endingText = "You can now quit the game by pressing ALT + F4 and fill out the questionnaire.";
 
-
     [SerializeField] private GameObject textElement;
+
+    public GameObject angryBee;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,6 +21,9 @@ public class EndTest : MonoBehaviour
             textElement.SetActive(true);
             textElement.GetComponent<Text>().fontSize = 25;
             textElement.GetComponent<Text>().text = endingText;
+
+            //Remove the chasing bee
+            Destroy(angryBee);
         }
     }
 }
