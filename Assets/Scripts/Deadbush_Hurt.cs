da8d76be52f82d5e6af16ee32b12821067d853c2
Sara886a <43111320+Sara886a@users.noreply.github.com>
2020-11-17 10:54:49 +0100

    Started implementing bush hurt trigger

---
+++ Assets/Scripts/Deadbush_Hurt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadbushTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject hurtableObject = other.GetComponent<HurtEffect>();
+
+        if (hurtableObject != null)
+        {
+            hurtableObject.Hit();
+        }
+    }
+}
