8d65c91f87350178b555457598a8f5ec24c2f31e
Sara886a <43111320+Sara886a@users.noreply.github.com>
2020-11-17 11:22:20 +0100

    removed timer from dead bush script

--- Assets/Scripts/Deadbush_Hurt.cs
+++ Assets/Scripts/Deadbush_Hurt.cs
@@ -4,26 +4,13 @@ using UnityEngine;
 
 public class DeadbushTrigger : MonoBehaviour
 {
-    private float currentTime;
-    public float waitTime;
-
-    void Start()
-    {
-        currentTime = Time.time - waitTime;
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         HurtEffect hurtableObject = other.GetComponent<HurtEffect>();
                 
         if (hurtableObject != null)
         {
-            if (currentTime + waitTime < Time.time)
-            {
-                currentTime = Time.time;
-                hurtableObject.Hit();
-            }
-            
+          hurtableObject.Hit();
         }
     }
 }
