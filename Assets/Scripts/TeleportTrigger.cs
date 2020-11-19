327981a05c32aad46a4f9eadbe1e81424fa1fc1d
Jokubas <jokubas@shape.dk>
2020-11-19 15:21:42 +0100

    Included a switch to deform position for HurtEffect. Included hurting to TeleportTrigger and updated the prefabs for them

--- Assets/Scripts/TeleportTrigger.cs
+++ Assets/Scripts/TeleportTrigger.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class TeleportTrigger : MonoBehaviour
 {
     public GameObject teleportDestination;  //where the player is taken when hitting this trigger
+    public bool shouldHurt = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (shouldHurt)
+        {
+            HurtEffect hurtableObject = other.GetComponent<HurtEffect>();
+
+            if (hurtableObject != null)
+            {
+                hurtableObject.Hit(false);
+            }
+        }
+    }
 }
