e05773aa67d551b18de5be37713bb8409e2bcbf0
latedude2 <simasc10@gmail.com>
2020-11-24 13:49:30 +0100

    Disable terrains that are not in view when inside cave

---
+++ Assets/Scripts/CullingTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+
+public class CullingTrigger : MonoBehaviour
+{
+    public GameObject[] gameObjectsToDisable;
+    public GameObject[] gameObjectsToEnable;
+}
